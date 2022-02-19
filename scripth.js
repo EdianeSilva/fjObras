17127e0fd85869881b25c3b76023d6b8e368dd84
Ediane Silva <ediane.web@gmail.com>
2022-02-19 10:32:36 -0300

    primeiro arquivo

---
+++ scripth.js
@@ -0,0 +1,47 @@
+//Jquery-->
+src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"
+// Materialize JavaScript -->
+src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"
+//menu Sidenav
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.materialboxed');
+    var instances = M.Materialbox.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.materialboxed').materialbox();
+  });
+  
+
+
+
+  // carrousel
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.slider');
+    var instances = M.Slider.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.slider').slider({
+      height:550 ,
+      duration: 700
+    });
+  });
+
+
+  //contato
+
+  document.querySelector('#contact-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    e.target.elements.name.value = '';
+    e.target.elements.email.value = '';
+    e.target.elements.message.value = '';
+  });
