e833207a2e3aed77edfd3c4b143fb433d91f6961
Ediane Silva <ediane.web@gmail.com>
2022-02-19 16:34:44 -0300

    adicionei ícone de whatsapp

--- scripth.js
+++ scripth.js
@@ -3,15 +3,6 @@ src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"
 // Materialize JavaScript -->
 src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"
 
-
-
-
-
-
-
-
-
-
 //menu Sidenav
 $(document).ready(function(){
     $('.sidenav').sidenav();
@@ -54,4 +45,17 @@ $(document).ready(function(){
     e.target.elements.name.value = '';
     e.target.elements.email.value = '';
     e.target.elements.message.value = '';
+  });
+
+  // BTN FLOATING
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.fixed-action-btn').floatingActionButton();
   });
